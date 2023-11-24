8112d82ad0fed2793d0ed230777ce730eab1fc10
Amanda Cristina <amandadaylan@gmail.com>
2023-11-24 15:12:40 -0300

    finalizando a estrutura de aplicação

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?auto=format&fit=crop&q=50&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+
+      <div className={styles.profile}>
+        <Avatar src="https://avatars.githubusercontent.com/u/95721308?v=4" />
+
+        <strong>Amanda Cristina</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
